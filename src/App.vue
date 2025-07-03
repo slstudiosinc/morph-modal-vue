55a2d3f1a028b464feae1330104bbd4135e454fa
Steve <slstudiosinc@gmail.com>
2025-07-03 00:15:49 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@
       <button class="mobile-hero-btn" id="mobileDetailsBtn">
         <span class="plus-circle">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="2" y="2" width="20" height="20" rx="4" stroke="#fff" stroke-width="2" fill="none"/>
             <path d="M12 7V17" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
             <path d="M7 12H17" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
           </svg>
